bf8d26bd9a05a90a0b29276ddb67d610b5d8cb78
Guillem Ferrer <guillemfd@gmail.com>
2021-12-02 23:18:49 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import contacts from "./contacts.json";
 import React, { useState } from "react";
+import Random from "./Random.js";
 
 
 function App() {
@@ -12,10 +13,27 @@ function App() {
   // const counter = myFirstState[0]
   // const setCounter = myFirstState[1]
 
-  const [contactList, setContactList] = useState(contacts.slice(0, 10));
+  const [contactList, setContactList] = useState(contacts.slice(0, 10)); //pongo 10 para ver alguna 🏆
+
+
+
+  // Iteration 3 ---- añadiendo actor random -------
+  const unseenContacts = contacts.slice(10); //nuevo array sin los 10 primeros
+
+  function randomContact() {  //función para crear un actor random
+    const random = Math.floor(Math.random() * unseenContacts.length);
+    // contactList.splice(random, 1);
+    return unseenContacts[random];
+  }
+  function addContact() {  //función para añadir actor random a la lista
+    setContactList(contactList.concat([randomContact()])); //xq no funciona con un push?
+  }
+
+
   return (
     <div className="App">
     <h1>IronContacts</h1>
+    <button onClick={addContact}>Add Random Contact</button>
       <table>
         <tr>
           <th>Picture</th>
