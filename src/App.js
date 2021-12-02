6dedc3de02d23eb665c4aeef324d4b38e029363b
Guillem Ferrer <guillemfd@gmail.com>
2021-12-02 12:37:11 +0100

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { useState } from "react";
+
 
 function App() {
+  // const myFirstState = useState(0)
+  // const contactList = myFirstState(contacts.slice(0, 5))
+  // const setContactList = myFirstState
+
+  // const myFirstState = useState(0)
+  // const counter = myFirstState[0]
+  // const setCounter = myFirstState[1]
+
+  const [contactList, setContactList] = useState(contacts.slice(0, 10));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>IronContacts</h1>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emy</th>
+        </tr>
+        {contactList.map((contact, i) => (
+        <tr key={i}>
+          <td><img src={contact.pictureUrl} alt={contact.name}/></td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity.toFixed(2)}</td>
+          <td>{contact.wonOscar === true ? "🏆" : ""}</td>
+          <td>{contact.wonEmmy === true ? "🏆" : ""}</td>
+        </tr>
+        ))}
+      </table>
     </div>
   );
 }
-
 export default App;
