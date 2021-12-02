1f7c2fdf049ee0e30d806a436bda6b04e5b28a8d
Guillem Ferrer <guillemfd@gmail.com>
2021-12-03 00:53:05 +0100

    Iteration 5 not working properly

--- src/App.js
+++ src/App.js
@@ -13,27 +13,63 @@ function App() {
   // const counter = myFirstState[0]
   // const setCounter = myFirstState[1]
 
+
+
+// Iteration 1 ----------------- slice ---------
   const [contactList, setContactList] = useState(contacts.slice(0, 10)); //pongo 10 para ver alguna 🏆
 
 
 
-  // Iteration 3 ---- añadiendo actor random -------
-  const unseenContacts = contacts.slice(10); //nuevo array sin los 10 primeros
+// Iteration 3 ---- añadiendo actor random -------
+const unseenContacts = contacts.slice(10); //nuevo array sin los 10 primeros
 
-  function randomContact() {  //función para crear un actor random
-    const random = Math.floor(Math.random() * unseenContacts.length);
-    // contactList.splice(random, 1);
-    return unseenContacts[random];
-  }
-  function addContact() {  //función para añadir actor random a la lista
-    setContactList(contactList.concat([randomContact()])); //xq no funciona con un push?
-  }
+function randomContact() {  //función para crear un actor random
+  const random = Math.floor(Math.random() * unseenContacts.length);
+  // contactList.splice(random, 1);
+  return unseenContacts[random];
+}
+function addContact() {  //función para añadir actor random a la lista
+  setContactList(contactList.concat([randomContact()])); //xq no funciona con un push?
+}
+
+
+
+//Iteration 4 ---------- Sorting By -----
+const sortByPopularity = () => {
+  const popularityList = contacts.sort((a, b) => {
+    return b.popularity - a.popularity;
+  });
+
+  setContactList([...popularityList]);  //...para crear un array nuevo sin modificar el inicial
+};
+
+const sortByName = () => {
+  const namesList = contacts.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+
+  setContactList([...namesList]);
+};
+
+
+//Iteration 5 ------------ Delete --------------- no hay manera de que funcione bien...-------------------
+const deleteContact = (id) => {
+    const filteredContacts = contacts.filter((contact) => {
+    return contact.id !== id;
+  });
+  setContactList(filteredContacts);
+};
+
+
 
 
   return (
     <div className="App">
     <h1>IronContacts</h1>
     <button onClick={addContact}>Add Random Contact</button>
+    <button onClick={sortByPopularity}>Sort by popularity</button>
+    <button onClick={sortByName}>Sort by name</button>
+
       <table>
         <tr>
           <th>Picture</th>
@@ -47,8 +83,12 @@ function App() {
           <td><img src={contact.pictureUrl} alt={contact.name}/></td>
           <td>{contact.name}</td>
           <td>{contact.popularity.toFixed(2)}</td>
+      {/* //Iteration 2 --------------------------------Iteration 2--------------- */}
           <td>{contact.wonOscar === true ? "🏆" : ""}</td>
           <td>{contact.wonEmmy === true ? "🏆" : ""}</td>
+          <td>
+            <button onClick={() => deleteContact(contact.id)}>Delete</button>
+          </td>
         </tr>
         ))}
       </table>
